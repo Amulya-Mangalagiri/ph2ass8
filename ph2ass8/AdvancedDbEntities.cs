bd449143fb7b87c43b172666db0431c9edf0cf95
Amulya <mangalagiriamulya18@gmail.com>
2023-08-23 08:42:18 +0530

    first Commit

---
+++ ph2ass8/AdvancedDbEntities.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class AdvancedDbEntities
+    {
+        public IEnumerable<Employee> Employees { get; internal set; }
+    }
+}
